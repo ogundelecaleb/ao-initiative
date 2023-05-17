675c5b92ef4383ebbb800190fe324c8580757c23
Ogundele Caleb <ogundelecaleb13@gmail.com>
2023-05-17 10:09:08 +0100

    newww

--- src/aboutUs.js
+++ src/aboutUs.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Footer from "./component/footer";
 import Header from "./component/header";
-import { motion as m } from "framer-motion";
+
 
 const AboutUs = () => {
   return (
