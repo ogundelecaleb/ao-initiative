a26b6ffd4ed6a339dc6fe9b4b4e3dcda2f34a770
Ogundele Caleb <ogundelecaleb13@gmail.com>
2023-07-09 02:13:17 +0100

    jj

--- src/App.js
+++ src/App.js
@@ -6,21 +6,22 @@ import Gallery from "./gallery";
 import Event from "./event";
 import Home from "./home";
 import SignUp from "./signUp";
-import "react-responsive-carousel/lib/styles/carousel.min.css"; 
+import Countdown from "./countdown";
+
+import "react-responsive-carousel/lib/styles/carousel.min.css";
 
 function App() {
   return (
-    
-      <BrowserRouter>
-        <Routes>
-          <Route index element={<Home />} />
-          <Route path="/signUp" element={<SignUp />} />
-          <Route path="/aboutUs" element={<AboutUs />} />
-          <Route path="/gallery" element={<Gallery />} />
-          <Route path="/event" element={<Event />} />
-        </Routes>
-      </BrowserRouter>
-    
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<Countdown />} />
+        <Route path="/signUp" element={<SignUp />} />
+        <Route path="/aboutUs" element={<AboutUs />} />
+        <Route path="/gallery" element={<Gallery />} />
+        <Route path="/event" element={<Event />} />
+        <Route path="/countdown" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
